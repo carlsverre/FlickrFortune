11183e69c11a006b7d1cdc9f5e8f887911dddee8
Carl Sverre <carl@7-up.(none)>
2009-05-08 15:59:01 -0700

    version 1 commit - moving from bzr

---
+++ flickrconfig.py
@@ -0,0 +1,40 @@
+
+# Config
+
+# Your flickr api key
+apiKey = "2e59ab73ca70e36739750dd0bca1f0db"
+
+# The xfce backdrop list
+xfce4BackgroundList = "/home/carl/.config/xfce4/desktop/backdrops.list"
+
+# Where to store the wallpapers
+wallpaperDir = "/home/carl/Pictures/"
+
+# The location of this file
+localDir = "/home/carl/dev/python/flickrfortune/"
+
+# the wallpaper prefix
+wallpaperPrefix = "wallpaper"
+
+# your screen size
+wallpaperSize = (1440,900)
+
+# the fontsize for the text
+fontsize = 25
+
+# Max errors before quitting
+maxErrors = 10
+
+# how to sort the pics
+sortType = ["interestingness-desc",
+            "interestingness-asc",
+            "relevance"][2]
+
+noiseWords = ["shakespeare",
+              "william",
+              "twain",
+              "wodehouse",
+              "ocasey",
+              "george",
+              "gobel",
+              "carlsverre"]
